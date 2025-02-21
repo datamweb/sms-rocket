c0ba3cdf150b4bd49942f09aa42ab89bf98601a0
Pooya Parsa Dadashi <Pooya_parsa_dadashi@yahoo.com>
2025-02-21 21:22:50 +0330

    chore: update phpstan ignoereErrors data

--- phpstan-baseline.php
+++ phpstan-baseline.php
@@ -8,20 +8,4 @@ $ignoreErrors = [];
 // 	'path' => __DIR__ . '/src/Drivers/AmootsmsDriver.php',
 // ];
 
-// $ignoreErrors[] = [
-// 	'message' => '#^Access to an undefined property hasProperty\\(status\\)\\:\\:\\$data\\.$#',
-// 	'count' => 3,
-// 	'path' => __DIR__ . '/src/Drivers/IdehpardazanDriver.php',
-// ];
-// $ignoreErrors[] = [
-// 	'message' => '#^Access to an undefined property hasProperty\\(status\\)\\:\\:\\$data\\.$#',
-// 	'count' => 2,
-// 	'path' => __DIR__ . '/src/Drivers/FarazsmsDriver.php',
-// ];
-// $ignoreErrors[] = [
-// 	'message' => '#Access to an undefined property hasProperty\(sid\)::\$status.#',
-// 	'count' => 1,
-// 	'path' => __DIR__ . '/src/Drivers/TwilioDriver.php',
-// ];
-
 return ['parameters' => ['ignoreErrors' => $ignoreErrors]];
